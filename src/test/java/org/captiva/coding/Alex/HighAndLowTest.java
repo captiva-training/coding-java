636def233a98736a9a99e5125592cdc7ace77568
Alvaro <aavila@zenfolio.com>
2019-10-11 14:46:21 -0400

    high_and_low_APBP

---
+++ src/test/java/org/captiva/coding/Alex/HighAndLowTest.java
@@ -0,0 +1,16 @@
+package org.captiva.coding.Alex;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class HighAndLowTest {
+    @Test
+    public void highAndLow()throws Exception {
+
+
+        Assert.assertEquals("42 -9", HighAndLow.highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+    }
+
+}
