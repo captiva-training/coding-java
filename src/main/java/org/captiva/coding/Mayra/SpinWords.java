5cee4abd3f9ff403edb407729719163d1c2a2121
MayraEcolqueC <mayra.colque.captiva@outlook.com>
2019-10-14 14:31:10 -0400

    SpinWords_Mayra

--- src/main/java/org/captiva/coding/Mayra/SpinWords.java
+++ src/main/java/org/captiva/coding/Mayra/SpinWords.java
@@ -1,12 +1,11 @@
 package org.captiva.coding.Mayra;
 
 public class SpinWords {
-    public String spinWords(String sentence) {
-        //TODO: Code stuff here
-
+    public String spinWords(final String sentence) {
+        final int limit = 5;
         String[] words = sentence.split(" ");
         for (int i = 0; i < words.length; i++) {
-            if (words[i].length() >= 5) {
+            if (words[i].length() >= limit) {
                 words[i] = new StringBuilder(words[i]).reverse().toString();
             }
         }
