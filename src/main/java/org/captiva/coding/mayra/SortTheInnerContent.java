ed40003360bff5d9c241b01fa896ca43aaf1ace6
Alvaro Avila <alvaro.avila@bairesdev.com>
2019-11-18 13:58:16 -0400

    sortTheInnerContent

---
+++ src/main/java/org/captiva/coding/mayra/SortTheInnerContent.java
@@ -0,0 +1,36 @@
+package mayra;
+public class sortTheInnerContent
+{
+    public static String sortTheInnerContent(String words)
+    {
+        StringBuilder cadena = new StringBuilder(words);
+        for (int i = 0; i < cadena.length(); i++)
+        {
+            int palabra = cadena.indexOf(" ", i);
+            if (palabra < 0)
+            {
+                palabra = cadena.length();
+            }
+            int inicio = i + 1;
+            int finP = palabra - 1;
+            reverseSortSubChars(cadena, inicio, finP);
+            i = palabra;
+        }
+        return cadena.toString();
+    }
+    public static void reverseSortSubChars(StringBuilder cadena, int inicio, int finP) {
+
+        for (int i = inicio; i < finP - 1; i++)
+        {
+            for (int j = i + 1; j < finP; j++)
+            {
+                if (cadena.charAt(i) < cadena.charAt(j))
+                {
+                    char temp = cadena.charAt(i);
+                    cadena.setCharAt(i, cadena.charAt(j));
+                    cadena.setCharAt(j, temp);
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/org/captiva/coding/mayra/SortTheInnerContentTest.java
@@ -0,0 +1,12 @@
+package mayra;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+class SolutionTest {
+    @Test
+    public void exampleTests() {
+        assertEquals("srot the inner ctonnet in dsnnieedcg oredr", sortTheInnerContent.sortTheInnerContent("sort the inner content in descending order"));
+        assertEquals("wiat for me", sortTheInnerContent.sortTheInnerContent("wait for me"));
+        assertEquals("tihs ktaa is esay", sortTheInnerContent.sortTheInnerContent("this kata is easy"));
+    }
+}
