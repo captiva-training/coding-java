5d1fbd6a7812ae3129e5aea1bef3944f32052c6f
Alvaro Avila <alvaro.avila@bairesdev.com>
2019-11-18 14:00:21 -0400

    sortTheInnerContent

--- src/main/java/org/captiva/coding/mayra/SortTheInnerContent.java
+++ src/main/java/org/captiva/coding/mayra/SortTheInnerContent.java
@@ -1,14 +1,15 @@
-package mayra;
-public class sortTheInnerContent
-{
-    public static String sortTheInnerContent(String words)
-    {
+package org.captiva.coding.mayra;
+
+public final class SortTheInnerContent {
+    private SortTheInnerContent() {
+
+    }
+
+    public static String sortTheInnerContent(final String words) {
         StringBuilder cadena = new StringBuilder(words);
-        for (int i = 0; i < cadena.length(); i++)
-        {
+        for (int i = 0; i < cadena.length(); i++) {
             int palabra = cadena.indexOf(" ", i);
-            if (palabra < 0)
-            {
+            if (palabra < 0) {
                 palabra = cadena.length();
             }
             int inicio = i + 1;
@@ -18,14 +19,12 @@ public class sortTheInnerContent
         }
         return cadena.toString();
     }
-    public static void reverseSortSubChars(StringBuilder cadena, int inicio, int finP) {
 
-        for (int i = inicio; i < finP - 1; i++)
-        {
-            for (int j = i + 1; j < finP; j++)
-            {
-                if (cadena.charAt(i) < cadena.charAt(j))
-                {
+    public static void reverseSortSubChars(final StringBuilder cadena, int inicio, int finP) {
+
+        for (int i = inicio; i < finP - 1; i++) {
+            for (int j = i + 1; j < finP; j++) {
+                if (cadena.charAt(i) < cadena.charAt(j)) {
                     char temp = cadena.charAt(i);
                     cadena.setCharAt(i, cadena.charAt(j));
                     cadena.setCharAt(j, temp);
--- src/test/java/org/captiva/coding/mayra/SortTheInnerContentTest.java
+++ src/test/java/org/captiva/coding/mayra/SortTheInnerContentTest.java
@@ -1,12 +1,15 @@
-package mayra;
+package org.captiva.coding.mayra;
+
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
-class SolutionTest {
+public class SortTheInnerContentTest {
     @Test
     public void exampleTests() {
-        assertEquals("srot the inner ctonnet in dsnnieedcg oredr", sortTheInnerContent.sortTheInnerContent("sort the inner content in descending order"));
-        assertEquals("wiat for me", sortTheInnerContent.sortTheInnerContent("wait for me"));
-        assertEquals("tihs ktaa is esay", sortTheInnerContent.sortTheInnerContent("this kata is easy"));
+        assertEquals("srot the inner ctonnet in dsnnieedcg oredr",
+                SortTheInnerContent.sortTheInnerContent("sort the inner content in descending order"));
+        assertEquals("wiat for me", SortTheInnerContent.sortTheInnerContent("wait for me"));
+        assertEquals("tihs ktaa is esay", SortTheInnerContent.sortTheInnerContent("this kata is easy"));
     }
 }
